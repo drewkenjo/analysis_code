ebcf92926816b29fbae07051b3a98d6a27eeda06
Sangbaek <sangbaek@mit.edu>
2019-09-05 14:38:54 -0400

    replace l with vector's mag

--- sangbaek/epg.groovy
+++ sangbaek/epg.groovy
@@ -24,7 +24,7 @@ while(reader.hasEvent()) {
   if (event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter")) {
     def (ele, pro, gam) = EPG.getEPG(event)*.particle
     def Vangle = {v1, v2 -> 
-       if( v1.mag() * v2.mag() !=0 && v1.dot(v2)<l1*l2 ) return Math.toDegrees( Math.acos(prod/(l1*l2) ) ); 
+       if( v1.mag() * v2.mag() !=0 && v1.dot(v2)<l1*l2 ) return Math.toDegrees( Math.acos(prod/(v1.mag() * v2.mag()) ) ); 
     }
 
     if(ele!=null) {
