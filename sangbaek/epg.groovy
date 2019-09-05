bf1ada338cda8061336e7795cd861ecb44eac91a
Sangbaek <sangbaek@mit.edu>
2019-09-05 14:36:05 -0400

    add some histograms at main

--- sangbaek/epg.groovy
+++ sangbaek/epg.groovy
@@ -10,6 +10,7 @@ import exclusive.EPG
 def hmm2_ep = new H1F("hmm2_ep", "missing mass squared, ep", 200,-2,4)
 def hmm2_eg = new H1F("hmm2_eg", "missing mass squared, eg", 200,-2,4)
 def hmm2_epg = new H1F("hmm2_epg", "missing mass squared, epg", 200,-2,4)
+def hangle_epg = new H1F("hangle_epg", "Angle between gamma and epX", 720,-360 ,360)
 
 def beam = new Particle(11, 0,0,10.6)//7.546)
 def target = new Particle(2212, 0,0,0)
@@ -22,6 +23,10 @@ while(reader.hasEvent()) {
   def event = reader.getNextEvent()
   if (event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter")) {
     def (ele, pro, gam) = EPG.getEPG(event)*.particle
+    def Vangle {v1, v2 -> 
+       if( v1.mag() * v2.mag() !=0 && v1.dot(v2)<l1*l2 ) return Math.toDegrees( Math.acos(prod/(l1*l2) ) ); 
+    }
+
     if(ele!=null) {
       def epX = new Particle(beam)
       epX.combine(target, 1)
@@ -39,9 +44,13 @@ while(reader.hasEvent()) {
       epgX.combine(pro,-1)
       epgX.combine(gam,-1)
 
+      mom_gam = gam.ele.vector().vect()
+      mom_epX = epX.ele.vector().vect()
+
       hmm2_ep.fill(epX.mass2())
       hmm2_eg.fill(egX.mass2())
       hmm2_epg.fill(epgX.mass2())
+      hangle_epg.fill(Vangle(mom_gam,mom_epX))
     }
   }
 }
@@ -55,4 +64,5 @@ out.cd('/epg')
 out.addDataSet(hmm2_ep)
 out.addDataSet(hmm2_epg)
 out.addDataSet(hmm2_eg)
+out.addDataSet(hangle_epg)
 out.writeFile('epg_out.hipo')
