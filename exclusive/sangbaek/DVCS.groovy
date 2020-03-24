d30ad6c253e0ed1b4a0a3b5d09454c189218a499
Sangbaek <sangbaek@mit.edu>
2020-03-23 20:07:59 -0400

    edit exclusivity cut part

--- exclusive/sangbaek/DVCS.groovy
+++ exclusive/sangbaek/DVCS.groovy
@@ -10,13 +10,9 @@ import pid.sangbaek.twogamma
 import pid.sangbaek.electron
 import org.jlab.clas.physics.LorentzVector;
 import event.Event
+import utils.KinTool
 
 class DVCS {
-
-
-  def Vangle = {v1, v2 -> 
-     if( v1.mag() * v2.mag() !=0 && v1.dot(v2)<v1.mag()*v2.mag() ) return Math.toDegrees( Math.acos(v1.dot(v2)/(v1.mag()*v2.mag()) ) ); 
-  }
 
   static def getEPG(event, electron_ind) {
 
@@ -40,7 +36,7 @@ class DVCS {
       inds.add(ind)
       
       // cut by pid
-      if(ind==null) return [null, null, null]
+      if(ind == null) return [null, null, null]
       // if(twogamma.findsecondGamma(event) >0) return [null,null,null]
     }
 
@@ -73,66 +69,15 @@ class DVCS {
 
   static def ExclCuts(VG1, VE, VMISS, VmissP, VmissG, Vhadr, Vhad2){
     // haz_g2==-1&& 
-     // VG1.e()>3
-     // && Vangle(VG1.vect(),VE.vect())>4
-     // && VMISS.e()<1.5 
-     // && VMISS.mass2() <0.2 && VMISS.mass2() >-0.2 
-     // && VmissP.mass2() < 3 && VmissP.mass2() > -0.25
-     // && VmissG.mass2() < 1 && VmissG.mass2() > -1
-     // && Math.sqrt(VMISS.px()*VMISS.px()+VMISS.py()*VMISS.py()) < 0.3
-     // && Vangle(VG1.vect(),VmissG.vect()) < 3
-     // && Vangle(Vhad2,Vhadr) < 25
-     //&& Vangle(Vhad2,Vhadr) < 90
-  }
-
-  static def getDVCS(ele, pro, gam, target, beam){
-
-    // cut by kinematics
-    def W_vec = new Particle(beam)
-    W_vec.combine(target, 1)
-    W_vec.combine(ele, -1)
-    def W = W_vec.mass()
-
-    def GS = new Particle(beam)
-    GS.combine(ele,-1)
-    def VGS = GS.vector()
-    def xB = -VGS.mass2()/(2*0.938*VGS.e())
-    def Q2 = -VGS.mass2()
-    if (W<2 || Q2<1 ||gam.e()<1) return [null, null, null]
-
-
-    // cut by exclusivity
-    def excl_cut = false
-
-    def VG1 = gam.vector()
-    def VE = ele.vector()
-    def VPROT = pro.vector()
-
-    def epX = new Particle(beam)
-    epX.combine(target, 1)
-    epX.combine(ele,-1)
-    epX.combine(pro,-1)
-    def VmissG = epX.vector()
-
-    def egX = new Particle(beam)
-    egX.combine(target, 1)
-    egX.combine(ele,-1)
-    egX.combine(gam,-1)
-    def VmissP = egX.vector()
-
-    def epgX = new Particle(beam)
-    epgX.combine(target, 1)
-    epgX.combine(ele,-1)
-    epgX.combine(pro,-1)
-    epgX.combine(gam,-1)
-    def VMISS = epgX.vector()
-
-    def Vhadr = (VPROT.vect()).cross(VGS.vect());
-    def Vhad2 = (VGS.vect()).cross(VG1.vect());
-
-
-    if (!excl_cut) return [null, null, null]
-
-    return (0..<3).collect{[particle:parts[it], pindex:inds[it], sector:secs[inds[it]]]}
+     (VG1.e() > 3 
+     && KinTool.Vangle(VG1.vect(),VE.vect())>4
+     && VMISS.e()<1.5 
+     && VMISS.mass2() <0.2 && VMISS.mass2() >-0.2 
+     && VmissP.mass2() < 3 && VmissP.mass2() > -0.25
+     && VmissG.mass2() < 1 && VmissG.mass2() > -1
+     && Math.sqrt(VMISS.px()*VMISS.px()+VMISS.py()*VMISS.py()) < 0.3
+     && KinTool.Vangle(VG1.vect(),VmissG.vect()) < 3
+     && KinTool.Vangle(Vhad2,Vhadr) < 25
+     && KinTool.Vangle(Vhad2,Vhadr) < 90)
   }
 }
