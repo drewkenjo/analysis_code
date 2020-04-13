5e2b07cbb3d19b6d7055bb177a6fc026486266fd
Sangbaek <sangbaek@mit.edu>
2020-04-13 00:44:23 -0400

    draw all DC hits, not only electrons'

--- sangbaek/draw_dcr.groovy
+++ sangbaek/draw_dcr.groovy
@@ -6,21 +6,27 @@ import java.util.concurrent.ConcurrentHashMap
 
 class draw_dcr {
 	def hists = new ConcurrentHashMap()
-	def h_DCR_hits = {new H2F("$it","$it",200,-100,100, 200,-100,100)}
+	def h_DCR_hits = {new H2F("$it","$it",200,-600,600, 200,-600,600)}
 
 	def processEvent(event) {
-		if (event.npart>0){
-			def electron_candidates = (0..<event.npart).findAll{event.pid.get(it)==11 && event.status.get(it)<0}
-			electron_candidates.each{ index ->
+		(0..<event.npart).each{ index ->
+			if (event.dc1_status.contains(index)){
 				def sec = event.dc_sector.get(index)-1
 				def hit1 = event.dc1.get(index).find{ hit -> hit.layer == 12}
 				if (hit1) hists.computeIfAbsent("/DCR1/sec_"+(sec+1),h_DCR_hits).fill(hit1.x, hit1.y)
+			}
+
+			if (event.dc2_status.contains(index)){
+				def sec = event.dc_sector.get(index)-1
 				def hit2 = event.dc2.get(index).find{ hit -> hit.layer == 24}
 				if (hit2) hists.computeIfAbsent("/DCR2/sec_"+(sec+1),h_DCR_hits).fill(hit2.x, hit2.y)
+			}
+
+			if (event.dc1_status.contains(index)){
+				def sec = event.dc_sector.get(index)-1
 				def hit3 = event.dc3.get(index).find{ hit -> hit.layer == 36}
 				if (hit3) hists.computeIfAbsent("/DCR3/sec_"+(sec+1),h_DCR_hits).fill(hit3.x, hit3.y)
 			}
 		}
 	}
-
 }
