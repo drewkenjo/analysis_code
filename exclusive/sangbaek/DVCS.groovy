b41a75010c98da0ca362d402078009392aefb5c7
Sangbaek <sangbaek@mit.edu>
2020-05-07 00:16:32 -0400

    add getEPG without fiducial cuts

--- exclusive/sangbaek/DVCS.groovy
+++ exclusive/sangbaek/DVCS.groovy
@@ -63,6 +63,50 @@ class DVCS {
     return (0..<3).collect{[particle:parts[it], pindex:inds[it], sector:secs[it]]}
   }
 
+  static def getEPG_EB(event) {
+
+    def findElectron = { ev -> (0..<ev.npart).find{ev.pid[it]==11 && ev.status[it]<0} }
+    def findProton = {ev -> (0..<ev.npart).findAll{ev.pid[it]==2212}
+      .max{ind -> (new Vector3(*[ev.px, ev.py, ev.pz].collect{it[ind]})).mag2()}
+    }
+    def findGamma = {ev -> (0..<ev.npart).findAll{ev.pid[it]==22}
+      .max{ind -> (new Vector3(*[ev.px, ev.py, ev.pz].collect{it[ind]})).mag2()}
+    }
+
+    def inds = []
+    for(def findPart in [findElectron, findProton, findGamma]) {
+      def ind = findPart(event)
+      inds.add(ind)
+      
+      // cut by pid
+      if(ind == null) return [null, null, null]
+      // if(twogamma.findsecondGamma(event) >0) return [null,null,null]
+    }
+
+    def secs1 = event.pcal_sector
+    def secs2 = event.ecal_inner_sector
+    def secs3 = event.ecal_outer_sector
+
+    def secs = []
+
+    (0..<3).each{
+      def sec1 = secs1[inds[it]]
+      def sec2 = secs2[inds[it]]
+      def sec3 = secs3[inds[it]]
+      if(sec1) secs.add(sec1)
+      else if(sec2) secs.add(sec2)
+      else if(sec3) secs.add(sec3)
+      else secs.add(null)
+    }
+
+    def parts = [11,2212,22].withIndex()
+      .collect{pid,i -> new Particle(pid, *[event.px, event.py, event.pz].collect{it[inds[i]]})
+    }
+
+    return (0..<3).collect{[particle:parts[it], pindex:inds[it], sector:secs[it]]}
+  }
+
+
   static def KineCuts(Q2, W, VG1){
       W>2 && Q2>1 && VG1.e() >1
   }
