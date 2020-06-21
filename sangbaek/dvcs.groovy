f6851df2828117cd775c072fff942f2001cf9e36
Sangbaek <sangbaek@mit.edu>
2020-06-21 13:34:16 -0400

    add h_t_trento

--- sangbaek/dvcs.groovy
+++ sangbaek/dvcs.groovy
@@ -38,6 +38,8 @@ class dvcs{
   def h_theta_trento = {new H2F("$it", "$it", 360, 0, 360, 100, 0, 100)}
   def h_phi_trento = {new H2F("$it", "$it", 360, 0, 360, 360, -180, 180)}
 
+  def h_t_trento =  {new H2F("$it", "$it", 360, 0, 360, 100, 0 , 4)}
+
   def h_Q2_xB = {new H2F("$it", "$it", 100, 0, 1,100, 0, 12)}
   def h_t_xB = {new H2F("$it", "$it", 100, 0, 1,100, 0, 2)}
   def h_Q2_t = {new H2F("$it", "$it", 100, 0, 4,100, 0, 12)}
@@ -367,7 +369,7 @@ class dvcs{
           hists.computeIfAbsent("/dvcs/corr/tmin", h_Q2_xB).fill(xB,Q2,tmin)
           hists.computeIfAbsent("/dvcs/corr/tcol", h_Q2_xB).fill(xB,Q2,tcol)
           hists.computeIfAbsent("/dvcs/binning/h_Q2_xB", h_Q2_xB).fill(xB,Q2)
-          hists.computeIfAbsent("/dvcs/binning/h_phi_trento", h_phi_trento).fill(t, TrentoAng)
+          hists.computeIfAbsent("/dvcs/binning/h_t_trento", h_t_trento).fill(TrentoAng, t)
           hists.computeIfAbsent("/dvcs/binning/h_Q2_t", h_Q2_t).fill(t,Q2)
 
           def pro_phi_convention = phi_convention(Math.toDegrees(pro.phi()-ele.phi()))
