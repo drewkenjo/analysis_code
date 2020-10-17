48a56aacd7d33356e22ea9e45738a49d108a033f
Sangbaek <sangbaek@mit.edu>
2020-10-17 17:57:35 -0400

    add phi and t for dvcs

--- utils/KinTool.groovy
+++ utils/KinTool.groovy
@@ -8,20 +8,33 @@ class KinTool{
     //move somplace else
 
     static def calcQ2(LorentzVector beam, LorentzVector measured_el){
-        LorentzVector VGS = new LorentzVector(beam)
-        VGS.sub(measured_el)
+        LorentzVector VGS = beam - measured_el
 	return -VGS.mass2()
     }
 
     static def calcXb(LorentzVector beam, LorentzVector measured_el){
-        LorentzVector VGS = new LorentzVector(beam)
-        VGS.sub(measured_el)
+        LorentzVector VGS = beam - measured_el
 	return calcQ2(beam,measured_el)/(2.0*PDGDatabase.getParticleMass(2212)*VGS.e())
     }
 
     static def calcT(LorentzVector measured_prot){
 	return 2.0*PDGDatabase.getParticleMass(2212)*(measured_prot.e()-PDGDatabase.getParticleMass(2212))
     }
+
+    static def calcT2(LorentzVector measured_el, LorentzVector measured_gam){
+    return 2.0*PDGDatabase.getParticleMass(2212)*(measured_prot.e()-PDGDatabase.getParticleMass(2212))
+    }
+
+    //used for DVCS
+    static def calcT2(LorentzVector beam, LorentzVector measured_el, LorentzVector measured_gam){
+        def Q2 = calcQ2(beam, measured_el)
+        def nu = calcNu(beam, measured_el)
+        LorentzVector VGS = beam - measured_el
+        def M = PDGDatabase.getParticleMass(2212)
+        def costheta = VGS.vect().dot(measured_gam.vect())/VGS.vect().mag()/measured_gam.vect().mag()
+        return (M*Q2+2*M*nu*(nu-Math.sqrt(nu*nu+Q2)*costheta))/(M+nu-Math.sqrt(nu*nu+Q2)*costheta)
+    }
+
 
     static def calcNu(LorentzVector beam, LorentzVector measured_el){
 	return calcQ2(beam,measured_el)/(2*PDGDatabase.getParticleMass(2212)*calcXb(beam,measured_el))
@@ -32,15 +45,26 @@ class KinTool{
     }
 
     static def calcPhiTrento(LorentzVector beam, LorentzVector measured_el,LorentzVector measured_prot){
-	def v3l = beam.vect().cross(measured_el.vect())
-    LorentzVector VGS = new LorentzVector(beam)
-    VGS.sub(measured_el)
-	def v3h = measured_prot.vect().cross(VGS.vect())
-	def trento = Math.toDegrees(Math.acos(v3l.dot(v3h)/(v3l.mag()*v3h.mag())))
+    def v3l = beam.vect().cross(measured_el.vect())
+    LorentzVector VGS = beam - measured_el
+    def v3h = measured_prot.vect().cross(VGS.vect())
+    def trento = Vangle(v3l, v3h)
 
-	if(v3l.dot(measured_prot.vect())<0){trento=360-trento}
-	return trento
+    if(v3l.dot(measured_prot.vect())>0){trento=360-trento}
+    return trento
     }
+
+    // used for DVCS.
+    static def calcPhiTrento2(LorentzVector beam, LorentzVector measured_el,LorentzVector measured_gam){
+    LorentzVector VGS = beam - measured_el
+    def v1 = VGS.vect().cross(measured_el.vect())
+    def v2 = VGS.vect().cross(measured_gam.vect())
+    def trento = KinTool.Vangle(v1,v2)
+
+    if (VGS.vect().dot(v1.cross(v2))<0) trento = 360 - trento
+    return trento
+    }
+
 
     static def delta_meas_energy( Double beam,  LorentzVector measured_el ){
 	def calc_e = beam/(1+ (beam/PDGDatabase.getParticleMass(2212))*(1-Math.cos(measured_el.theta())) );
@@ -52,9 +76,11 @@ class KinTool{
 	return Math.toDegrees((calc_theta  - measured_el.theta() ))
     }
 
+    static def Vangle = {v1, v2 -> 
+        if( v1.mag() * v2.mag() !=0 && v1.dot(v2)<v1.mag()*v2.mag() ) return Math.toDegrees( Math.acos(v1.dot(v2)/(v1.mag()*v2.mag()) ) ); 
+        else return 0
+    }
     
-    
-
 }
 
 
